2cb010d6cf8db28009ba0bca522e38bb03d0a0b8
ephemeralCode <destroid707@yandex.ru>
2021-10-04 14:26:34 +0300

    fix: change solve st-encode-line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -11,16 +11,33 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default function encodeLine(str) {
-  throw new NotImplementedError('Not implemented');
-  let hashMap = {}
-  let arr = []
+  // throw new NotImplementedError('Not implemented');
+  // let hashMap = {}
+  // let arr = []
 
-  if (typeof(str) !== 'string') return ''
+  // if (typeof(str) !== 'string') return ''
 
-  // str.split('').map(elem => hashMap[elem] ? hashMap[elem]++ : hashMap[elem] = 1)
-  // Object.entries(hashMap).forEach(elem => arr.push(`${elem[1]}${elem[0]}`))
+  // str.split('').map(elem => hashMap[elem] ? 
+  //   hashMap[elem]++ : hashMap[elem] = 1)
+
+  // Object.entries(hashMap).forEach(elem => {
+  //   arr.push(`${elem[1]}${elem[0]}`)
+  // })
+
   // return arr.join('')
-  
-  str.split('')
 
+
+
+  //! 'abbcca' ok_hand...
+  let arr = str
+
+  for(let i = 0; i < str.length; i++){
+      let target = arr.match(/(\w)\1{1,}/)
+
+      if(!target) break
+
+      arr = arr.replace(/(\w)\1{1,}/, `${target[0].length}${target[1]}`)
+  }
+
+  return arr
 }
